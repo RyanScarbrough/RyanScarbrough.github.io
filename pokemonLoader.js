2d1123d90cb5c8c211e22aa2072140087c25c3ec
RyanScarbrough <RyanScarbrough@Gmail.com>
2022-03-26 15:51:41 -0700

    Upload

---
+++ pokemonLoader.js
@@ -0,0 +1,23 @@
+function loadPokemon() {
+   const howManyInput = document.getElementById("howMany")
+   const howManyValue = howManyInput.value;
+   
+   let newHtml = "";
+   for (let i = 1; i <= howManyValue; i++) {
+      iString = String(i);
+      while (iString.length < 4) iString = "0" + iString; // 0001, 0002, etc.
+
+      newHtml += `<div class="pokemonTile">`;
+      newHtml += `#${i}<br>`;
+      newHtml += `<img src="pokemonImages/${iString}.png" loading="lazy" alt="${pokemonNames[i]}">`;
+      newHtml += `<br>${pokemonNames[i]}`;
+      newHtml += `</div>`;
+   }
+
+   document.getElementById("pokemonContainer").innerHTML = newHtml;
+
+   howManyInput.value = "";
+   howManyInput.blur();
+
+   return false; // prevents form from refreshing page.
+}
